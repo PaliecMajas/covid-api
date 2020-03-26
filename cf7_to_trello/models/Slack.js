ba537c125677492c83a2b0806d2349b41f2abbf2
Uldis Jansons <uj@mobilly.lv>
2020-03-26 15:13:05 +0200

    Add extra slack webhook call

--- cf7_to_trello/models/Slack.js
+++ cf7_to_trello/models/Slack.js
@@ -3,12 +3,14 @@ const axios = require('axios');
 
 class Slack {
     async newMessage(authorName, cardUri) {
-        const uri = 'https://hooks.slack.com/services/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';
+        const hackForceSlackUri = 'https://hooks.slack.com/services/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';
+        const paliecMajasSlackUri = 'https://hooks.slack.com/services/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';
         const data = {
             text: `New order placed! From: ${authorName}. Go check it out ${cardUri}`
         };
         console.log(`[D] Slack webhook call ${JSON.stringify(data)} -> ${uri}`);
-        await axios.post(uri, data);
+        await axios.post(hackForceSlackUri, data);
+        await axios.post(paliecMajasSlackUri, data);
     }
 }
 
