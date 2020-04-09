42cc477f7e7ac0f03a6533dd1eba3241b33be565
Daniel Zohar <danielzohar@users.noreply.github.com>
2020-04-09 14:49:39 +0300

    Replace hardcoded Slack url with config (#2)

--- cf7_to_trello/models/Slack.js
+++ cf7_to_trello/models/Slack.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 class Slack {
     async newMessage(authorName, cardUri) {
-        const paliecMajasSlackUri = 'https://hooks.slack.com/services/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';
+        const paliecMajasSlackUri = process.env.SLACK_HOOK_URL;
         const data = {
             text: `New order placed! From: ${authorName}. Go check it out ${cardUri}`
         };
