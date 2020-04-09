2edcc2d6675da85f1ea59ec4005b197a8c85ba7d
Daniel Zohar <danielzohar@users.noreply.github.com>
2020-04-09 10:12:51 +0300

    Remove hackforce slack hook (#1)
    https://trello.com/c/xZu3g1by/57-remove-integrations-from-old-slack-hackforce
    * Stop logging delivery messages onto HackForce Slack channel
    * Clean up dead code
    * Remove unused file
    * Tidy up code

--- cf7_to_trello/models/Slack.js
+++ cf7_to_trello/models/Slack.js
@@ -1,15 +1,11 @@
 const axios = require('axios');
 
-
 class Slack {
     async newMessage(authorName, cardUri) {
-        const hackForceSlackUri = 'https://hooks.slack.com/services/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';
         const paliecMajasSlackUri = 'https://hooks.slack.com/services/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';
         const data = {
             text: `New order placed! From: ${authorName}. Go check it out ${cardUri}`
         };
-        console.log(`[D] Slack webhook call ${JSON.stringify(data)} -> ${hackForceSlackUri}`);
-        await axios.post(hackForceSlackUri, data);
         console.log(`[D] Slack webhook call ${JSON.stringify(data)} -> ${paliecMajasSlackUri}`);
         await axios.post(paliecMajasSlackUri, data);
     }
